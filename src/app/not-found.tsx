f5a0522eb130a1bbf990971f74bb2661f4e65310
Ville Nupponen <ville.nupponen@aika.in>
2024-12-25 23:36:51 +0200

    Add 404 error pages tracking with plausible

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,11 @@
+import NotFoundLogger from '@/components/notFoundLogger';
+
 import styles from './styles.module.scss';
 
 const Page = () => (
     <div className={styles.notFound}>
         <h1>There is nothing here ...</h1>
+        <NotFoundLogger />
     </div>
 );
 
