94242926439623db7eff7cce9e2faf674b2cbfce
Ville Nupponen <ville.nupponen@aika.in>
2024-12-25 23:41:07 +0200

    Fix 404 page styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,22 @@
+import Link from 'next/link';
+
+import ArrowBackIcon from '@/icons/arrow-back.svg';
+
 import NotFoundLogger from '@/components/notFoundLogger';
 
 import styles from './styles.module.scss';
 
 const Page = () => (
-    <div className={styles.notFound}>
-        <h1>There is nothing here ...</h1>
+    <>
+        <div className={styles.backToHomePage}>
+            <Link href='/'>
+                <ArrowBackIcon />
+            </Link>
+        </div>
+        <div className={styles.notFound}>
+            <h1>There is nothing here ...</h1>
+        </div>
         <NotFoundLogger />
-    </div>
+    </>
 );
-
 export default Page;
